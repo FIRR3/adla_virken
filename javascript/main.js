b534ca8087ad43c85462f9470af8c5e376ffdf1e
FIRR3 <150165932+FIRR3@users.noreply.github.com>
2025-01-19 01:25:48 +0100

    Responsive navbar
    Finished up the menu button (still work in progress), and added functionality with dropdowns.

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+// menu for mobile
+const menu = document.getElementById("menu")
+
+menu.style.top = '-' + menu.offsetHeight + 'px';
+
+const openMenu = () =>{
+  menu.style.top = 0 + 'px';
+}
+const closeMenu = () =>{
+  menu.style.top = '-' + menu.offsetHeight + 'px';
+}
+
+// navbar dropdowns
+let dropdownActivators = document.querySelectorAll(".dropdownActivator");
+
+dropdownActivators.forEach(activator =>{
+  activator.addEventListener("click", () =>{
+    if(activator.querySelector(".dropdown").classList.contains("flex")){
+      activator.querySelector(".dropdown").classList.replace("flex", "hidden")
+    }
+    else{
+      activator.querySelector(".dropdown").classList.replace("hidden", "flex")
+    }
+  })
+})

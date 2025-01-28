c28e360eaf9f28662f698145ffd2a005a5cdf92f
FIRR3 <150165932+FIRR3@users.noreply.github.com>
2025-01-28 20:06:56 +0100

    Hero section
    Navbar and hero section nearly complete

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,41 @@
-const bar = document.getElementById("bar");
-const menu = document.getElementById("menu");
-bar.addEventListener("click", () =>{
-  console.log("hey")
-  if(menu.classList.contains("max-lg:h-0")){
-    menu.classList.replace("max-lg:h-0", "max-lg:min-h-32")
+bar.addEventListener("click", () => {
+  const bar = document.getElementById("bar");
+  const menu = document.getElementById("menu");
+
+  //dropdown menu
+  menu.classList.contains("max-lg:h-0") ? menu.classList.replace("max-lg:h-0", "max-lg:h-auto") : menu.classList.replace("max-lg:h-auto", "max-lg:h-0");
+  menu.classList.contains("max-lg:overflow-hidden") ? menu.classList.replace("max-lg:overflow-hidden", "max-lg:overflow-auto") : menu.classList.replace("max-lg:overflow-auto", "max-lg:overflow-hidden");
+
+  //change from bar to "x" on active menu
+  bar.querySelector("i").classList.contains("fa-bars") ? bar.querySelector("i").classList.replace("fa-bars", "fa-xmark") : bar.querySelector("i").classList.replace("fa-xmark", "fa-bars");
+});
+
+// navbar dropdowns
+let dropdownActivators = document.querySelectorAll(".dropdown-activator");
+
+const toggleDropDown = (dropdown) => {
+  if (dropdown.querySelector(".dropdown").classList.contains("flex")) {
+    dropdown.querySelector(".dropdown").classList.replace("flex", "hidden");
+  } else {
+    dropdown.querySelector(".dropdown").classList.replace("hidden", "flex");
   }
-})
+};
+
+dropdownActivators.forEach((activator) => {
+  activator.addEventListener("click", () => {
+    //show or hide nav dropdowns when clicked
+    toggleDropDown(activator);
+    //set grey background color for dropdown-header if the dropdown is active
+    activator.querySelector(".dropdown-header").classList.toggle("lg:bg-grey-light");
+  });
+  //close nav dropdown if user clicks on window
+  //doesn't apply on mobile
+  window.addEventListener("click", (e) => {
+    if (!activator.querySelector(".dropdown-header").contains(e.target) && !activator.querySelector(".dropdown").contains(e.target)) {
+      if (activator.querySelector(".dropdown").classList.contains("flex")) {
+        activator.querySelector(".dropdown").classList.replace("flex", "hidden");
+        activator.querySelector(".dropdown-header").classList.toggle("lg:bg-grey-light");
+      }
+    }
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,41 @@
-import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
+import fluid, { extract, screens, fontSize } from "fluid-tailwind";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: {
-    files:   ["./public/*.html", "./javascript/*.js", "./src/*.css"],
+    files: ["./public/*.html", "./javascript/*.js", "./src/*.css"],
     extract,
   },
   theme: {
     screens, // Tailwind's default screens, in `rem`
     fontSize, // Tailwind's default font sizes, in `rem` (including line heights)
     colors: {
-      'primary':{
-        "full": "#6fab4e", //rgba(111, 171, 78, 1)
-        "transparent": "#7FB164", //"rgba(111, 171, 78, 0.9)"
+      primary: {
+        full: "#6fab4e", //rgba(111, 171, 78, 1)
+        transparent: "#7FB164", //"rgba(111, 171, 78, 0.9)"
       },
-      'secondary': "#BEE7A8",
+      secondary: "#BEE7A8",
 
-      'white': "#ffffff",
-      'black': "#000000",
-      'grey': "#909090",
-      'grey-light': "#ebebeb", //"rgba(144, 144, 144, 0.15)"
+      white: "#ffffff",
+      black: "#000000",
+      grey: "#909090",
+      "grey-light": "#ebebeb", //"rgba(144, 144, 144, 0.15)"
 
-      'transparent': "00FFFFFF",
+      transparent: "00FFFFFF",
     },
-    fontFamily:{
-      'manrope': 'Manrope, sans-serif',
-      'noto-sans': 'Noto sans, sans-serif',
+    fontFamily: {
+      manrope: "Manrope, sans-serif",
+      "noto-sans": "Noto sans, sans-serif",
     },
     extend: {
       screens: {
-        xs: '20rem'
+        xs: "20rem",
       },
-      fontSize: {
-        'header': '3.5rem',
-        'sub': '2.25rem',
-        'paragraph': '1rem',
-      },
-      
     },
   },
   plugins: [
     fluid({
-      checkSC144: false // default: true
-    })
+      checkSC144: false, // default: true
+    }),
   ],
-}
-
+};
